c7f86c2a5e96171c1ed8d20917e48cec7fb7bd65
beuchety <beuchety@churchofjesuschrist.org>
2020-01-21 10:47:47 -0700

    Lecture 52 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipieListComponent } from './recipies/recipie-list/recipie-list.component';
+import { RecipieItemComponent } from './recipies/recipie-list/recipie-item/recipie-item.component';
+import { RecipieDetailComponent } from './recipies/recipie-detail/recipie-detail.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { ShoppingListItemComponent } from './shopping-list/shopping-list-item/shopping-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipieListComponent,
+    RecipieItemComponent,
+    RecipieDetailComponent,
+    RecipiesComponent,
+    ShoppingListItemComponent
   ],
   imports: [
     BrowserModule
